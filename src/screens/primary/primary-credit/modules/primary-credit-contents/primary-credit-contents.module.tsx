c3de1b7740d6c9b14416daf42fd12d8a3964a1b2
DongHyunKim <akainoo123@gmail.com>
2023-11-27 13:40:08 +0900

    feat : primary-credit 3차 구현

--- src/screens/primary/primary-credit/modules/primary-credit-contents/primary-credit-contents.module.tsx
+++ src/screens/primary/primary-credit/modules/primary-credit-contents/primary-credit-contents.module.tsx
@@ -1,75 +1,115 @@
 import { Stack, Columns, Column } from '@mobily/stacks';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { useDidUpdate } from 'rooks';
 
 import { CREDITS } from './primary-credit-content.const';
 
-import { Icon, Text } from '@/atoms';
+import { Dialog, Icon, Text } from '@/atoms';
 import { palette } from '@/utils';
 
 type PrimaryCreditContentsModuleProps = {};
 
 export const PrimaryCreditContentsModule =
   memo<PrimaryCreditContentsModuleProps>(() => {
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [selectedCredit, setSelectedCredit] = useState({
+      credit: 0,
+      price: '0',
+    });
+
+    const closeModal = () => {
+      setDialogVisible(false);
+    };
+
+    const handlePressCancelButton = () => {
+      closeModal();
+    };
+    const handlePressOkayButton = () => {
+      closeModal();
+    };
+
+    useDidUpdate(() => {
+      setDialogVisible(true);
+    }, [selectedCredit]);
+
     return (
-      <Stack space={12} paddingTop={48}>
-        <Stack horizontal space={8} align="center">
-          <Text fontWeight="700" fontSize="16" color="gray-900">
-            보유 크래딧
-          </Text>
-          <Stack horizontal space={4} align="center">
+      <>
+        <Dialog
+          dialogVisible={dialogVisible}
+          title="크레딧 결제"
+          content={`${selectedCredit.credit} 크레딧 - ${selectedCredit.price} 결제하시겠습니까?`}
+          cancelButton={{ label: '취소', onPress: handlePressCancelButton }}
+          okayButton={{ label: '확인', onPress: handlePressOkayButton }}
+          onBackdropPress={closeModal}
+        />
+        <Stack space={12} paddingTop={48}>
+          <Stack horizontal space={8} align="center">
             <Text fontWeight="700" fontSize="16" color="gray-900">
-              0
+              보유 크래딧
             </Text>
-            <Icon
-              name="monetization-on"
-              color={palette['secondary']}
-              size={20}
-            />
+            <Stack horizontal space={4} align="center">
+              <Text fontWeight="700" fontSize="16" color="gray-900">
+                0
+              </Text>
+              <Icon
+                name="monetization-on"
+                color={palette['secondary']}
+                size={20}
+              />
+            </Stack>
           </Stack>
-        </Stack>
-        {CREDITS.map(v => {
-          return (
-            <TouchableOpacity
-              style={{
-                padding: 12,
-                borderRadius: 4,
-                backgroundColor: palette['gray-200'],
-              }}
-              key={v.credit}>
-              <Columns>
-                <Column width="fluid">
-                  <Stack horizontal space={4} align="center">
-                    <Text fontWeight="400" fontSize="16" color="secondary">
-                      {v.credit}
-                    </Text>
-                    <Icon
-                      name="monetization-on"
-                      color={palette['secondary']}
-                      size={20}
-                    />
-                  </Stack>
-                </Column>
-                <Column width="content">
-                  <Stack horizontal space={8} align="center">
-                    {v.originalPrice && (
-                      <Text
-                        fontWeight="400"
-                        fontSize="16"
-                        color="gray-900"
-                        style={{ textDecorationLine: 'line-through' }}>
-                        {v.originalPrice}
+          {CREDITS.map(v => {
+            const handlePressCreditItem = () => {
+              setSelectedCredit({
+                credit: v.credit,
+                price: v.price,
+              });
+            };
+
+            return (
+              <TouchableOpacity
+                onPress={handlePressCreditItem}
+                style={{
+                  padding: 12,
+                  borderRadius: 4,
+                  backgroundColor: palette['gray-200'],
+                }}
+                key={v.credit}>
+                <Columns>
+                  <Column width="fluid">
+                    <Stack horizontal space={4} align="center">
+                      <Text fontWeight="400" fontSize="16" color="secondary">
+                        {v.credit}
                       </Text>
-                    )}
-                    <Text fontWeight="400" fontSize="16" color="gray-900">
-                      {v.price}
-                    </Text>
-                  </Stack>
-                </Column>
-              </Columns>
-            </TouchableOpacity>
-          );
-        })}
-      </Stack>
+                      <Icon
+                        name="monetization-on"
+                        color={palette['secondary']}
+                        size={20}
+                      />
+                    </Stack>
+                  </Column>
+                  <Column width="content">
+                    <Stack horizontal space={8} align="center">
+                      {v.originalPrice && (
+                        <Text
+                          fontWeight="400"
+                          fontSize="16"
+                          color="gray-900"
+                          style={{ textDecorationLine: 'line-through' }}>
+                          {v.originalPrice}
+                        </Text>
+                      )}
+                      <Text fontWeight="400" fontSize="16" color="gray-900">
+                        {v.price}
+                      </Text>
+                    </Stack>
+                  </Column>
+                </Columns>
+              </TouchableOpacity>
+            );
+          })}
+        </Stack>
+      </>
     );
   });
