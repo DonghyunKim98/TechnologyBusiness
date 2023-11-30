5e6647316ffa577bb94d1f38f9496e9f164a2fde
DongHyunKim <akainoo123@gmail.com>
2023-11-30 16:28:06 +0900

    feat : facility-reservation 5차 구현

---
+++ src/screens/facility-reservation/modules/components/time-reservation/time-reservation.component.tsx
@@ -0,0 +1,41 @@
+import sample from 'lodash/sample';
+import { memo } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { Text } from '@/atoms';
+import { palette } from '@/utils';
+
+type TimeReservationComponentProps = {
+  time: string;
+  onPress: () => void;
+};
+
+export const TimeReservationComponent = memo<TimeReservationComponentProps>(
+  ({ time, onPress }) => {
+    const isAvailableByRandom = sample([true, false]);
+
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        disabled={!isAvailableByRandom}
+        style={[
+          {
+            borderRadius: 8,
+            paddingHorizontal: 20,
+            paddingVertical: 12,
+            backgroundColor: palette['primary'],
+          },
+          !isAvailableByRandom && {
+            backgroundColor: palette['gray-200'],
+          },
+        ]}>
+        <Text
+          fontWeight="400"
+          fontSize="14"
+          color={isAvailableByRandom ? 'white' : 'gray-400'}>
+          {time}
+        </Text>
+      </TouchableOpacity>
+    );
+  },
+);
--- src/screens/facility-reservation/modules/facility-reservation-contents/facility-reservation-contents.module.tsx
+++ src/screens/facility-reservation/modules/facility-reservation-contents/facility-reservation-contents.module.tsx
@@ -1,16 +1,22 @@
 import { Box, Stack } from '@mobily/stacks';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import dayjs from 'dayjs';
-import sample from 'lodash/sample';
 import { memo, useState } from 'react';
-import { TouchableOpacity } from 'react-native';
 import { Calendar, DateData, LocaleConfig } from 'react-native-calendars';
+import { useDidUpdate } from 'rooks';
+
+import {
+  FacilityReservationScreenNavigationProps,
+  FacilityReservationScreenNavigationRouteProps,
+} from '../../facility-reservation.screen';
+import { TimeReservationComponent } from '../components';
 
 import {
   AFTERNOON_TIMES,
   MORNING_TIMES,
 } from './facility-reservation-contents.const';
 
-import { Text } from '@/atoms';
+import { Dialog, SingleButtonProps, Text } from '@/atoms';
 import { palette } from '@/utils';
 
 type FacilityReservationContentsModuleProps = {};
@@ -52,11 +58,57 @@ LocaleConfig.defaultLocale = 'kr';
 
 export const FacilityReservationContentsModule =
   memo<FacilityReservationContentsModuleProps>(() => {
+    const {
+      params: { title },
+    } = useRoute<FacilityReservationScreenNavigationRouteProps>();
+    const navigation =
+      useNavigation<FacilityReservationScreenNavigationProps>();
+
     const [selectedDate, setSelectedDate] = useState<null | string>(null);
+    const [dialogVisible, setDialogVisible] = useState(false);
+    const [selectedTime, setSelectedTime] = useState({
+      time: '',
+      pressed: 0,
+    });
+    const [isFirstConfirmButtonPressed, setIsFirstConfirmButtonPressed] =
+      useState(false);
 
     const handlePressDay = (day: DateData) => {
       setSelectedDate(day.dateString);
     };
+
+    useDidUpdate(() => {
+      setDialogVisible(true);
+    }, [selectedTime]);
+
+    const closeDialog = () => {
+      setDialogVisible(false);
+    };
+
+    const handlePressCancelButton = () => {
+      closeDialog();
+    };
+
+    const handlePressFirstConfirmButton = () => {
+      setIsFirstConfirmButtonPressed(true);
+    };
+
+    const handlePressSecondConfirmButton = () => {
+      closeDialog();
+      navigation.reset({
+        index: 1,
+        routes: [
+          {
+            name: 'PrimaryStack',
+            params: { screen: 'PrimaryExerciseScheduleScreen' },
+          },
+        ],
+      });
+    };
+
+    const mapOkayButton: SingleButtonProps = isFirstConfirmButtonPressed
+      ? { label: '일정 확인하기', onPress: handlePressSecondConfirmButton }
+      : { label: '확인', onPress: handlePressFirstConfirmButton };
 
     return (
       <Stack marginTop={16} space={24}>
@@ -99,31 +151,19 @@ export const FacilityReservationContentsModule =
               alignX="between"
               wrap="wrap"
               style={{ gap: 8 }}>
-              {MORNING_TIMES.map(v => {
-                const isAvailableByRandom = sample([true, false]);
+              {MORNING_TIMES.map(time => {
+                const handlePressTimeReservation = () => {
+                  setSelectedTime(prev => ({
+                    time: `오전 ${time}`,
+                    pressed: prev.pressed + 1,
+                  }));
+                };
 
                 return (
-                  <TouchableOpacity
-                    key={v}
-                    disabled={!isAvailableByRandom}
-                    style={[
-                      {
-                        borderRadius: 8,
-                        paddingHorizontal: 20,
-                        paddingVertical: 12,
-                        backgroundColor: palette['primary'],
-                      },
-                      !isAvailableByRandom && {
-                        backgroundColor: palette['gray-200'],
-                      },
-                    ]}>
-                    <Text
-                      fontWeight="400"
-                      fontSize="14"
-                      color={isAvailableByRandom ? 'white' : 'gray-400'}>
-                      {v}
-                    </Text>
-                  </TouchableOpacity>
+                  <TimeReservationComponent
+                    time={time}
+                    onPress={handlePressTimeReservation}
+                  />
                 );
               })}
             </Box>
@@ -139,36 +179,37 @@ export const FacilityReservationContentsModule =
               alignX="between"
               wrap="wrap"
               style={{ gap: 8 }}>
-              {AFTERNOON_TIMES.map(v => {
-                const isAvailableByRandom = sample([true, false]);
+              {AFTERNOON_TIMES.map(time => {
+                const handlePressTimeReservation = () => {
+                  setSelectedTime(prev => ({
+                    time: `오전 ${time}`,
+                    pressed: prev.pressed + 1,
+                  }));
+                };
 
                 return (
-                  <TouchableOpacity
-                    key={v}
-                    disabled={!isAvailableByRandom}
-                    style={[
-                      {
-                        borderRadius: 8,
-                        paddingHorizontal: 20,
-                        paddingVertical: 12,
-                        backgroundColor: palette['primary'],
-                      },
-                      !isAvailableByRandom && {
-                        backgroundColor: palette['gray-200'],
-                      },
-                    ]}>
-                    <Text
-                      fontWeight="400"
-                      fontSize="14"
-                      color={isAvailableByRandom ? 'white' : 'gray-400'}>
-                      {v}
-                    </Text>
-                  </TouchableOpacity>
+                  <TimeReservationComponent
+                    time={time}
+                    onPress={handlePressTimeReservation}
+                  />
                 );
               })}
             </Box>
           </Stack>
         )}
+        <Dialog
+          dialogVisible={dialogVisible}
+          title={`${title} ${dayjs(selectedDate).format('MM월 DD일')} ${
+            selectedTime.time
+          } 예약하시겠습니까? 1크레딧이 차감됩니다`}
+          cancelButton={
+            isFirstConfirmButtonPressed
+              ? undefined
+              : { label: '취소', onPress: handlePressCancelButton }
+          }
+          okayButton={mapOkayButton}
+          onBackdropPress={closeDialog}
+        />
       </Stack>
     );
   });
