aa57b275328a7fb06054be8689e4fbaa7e86cdbf
DongHyunKim <akainoo123@gmail.com>
2023-11-30 16:11:41 +0900

    feat: facility-reservation 4차 구현

--- src/screens/facility-reservation/modules/facility-reservation-contents/facility-reservation-contents.module.tsx
+++ src/screens/facility-reservation/modules/facility-reservation-contents/facility-reservation-contents.module.tsx
@@ -1,8 +1,16 @@
-import { Box } from '@mobily/stacks';
+import { Box, Stack } from '@mobily/stacks';
 import dayjs from 'dayjs';
+import sample from 'lodash/sample';
 import { memo, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { Calendar, DateData, LocaleConfig } from 'react-native-calendars';
 
+import {
+  AFTERNOON_TIMES,
+  MORNING_TIMES,
+} from './facility-reservation-contents.const';
+
+import { Text } from '@/atoms';
 import { palette } from '@/utils';
 
 type FacilityReservationContentsModuleProps = {};
@@ -51,35 +59,116 @@ export const FacilityReservationContentsModule =
     };
 
     return (
-      <Box
-        marginTop={16}
-        style={{ backgroundColor: palette['gray-200'], borderRadius: 8 }}
-        paddingY={16}>
-        <Calendar
-          style={{ backgroundColor: palette['gray-200'] }}
-          monthFormat={'yyyy년 MM월'}
-          onDayPress={handlePressDay}
-          hideExtraDays={true}
-          maxDate={dayjs().add(1, 'M').format('YYYY-MM-DD')}
-          minDate={dayjs().format('YYYY-MM-DD')}
-          markedDates={{
-            ...(selectedDate && {
-              [selectedDate]: {
-                selected: true,
-                disableTouchEvent: true,
-                selectedColor: palette['gray-400'],
-                selectedTextColor: palette['gray-900'],
-              },
-            }),
-          }}
-          theme={{
-            calendarBackground: palette['gray-200'],
-            backgroundColor: palette['gray-200'],
-            todayTextColor: palette['gray-800'],
-            textDayFontFamily: 'Roboto-Medium',
-            textMonthFontFamily: 'Roboto-Medium',
-          }}
-        />
-      </Box>
+      <Stack marginTop={16} space={24}>
+        <Box
+          style={{ backgroundColor: palette['gray-200'], borderRadius: 8 }}
+          paddingY={16}>
+          <Calendar
+            style={{ backgroundColor: palette['gray-200'] }}
+            monthFormat={'yyyy년 MM월'}
+            onDayPress={handlePressDay}
+            hideExtraDays={true}
+            maxDate={dayjs().add(1, 'M').format('YYYY-MM-DD')}
+            minDate={dayjs().format('YYYY-MM-DD')}
+            markedDates={{
+              ...(selectedDate && {
+                [selectedDate]: {
+                  selected: true,
+                  disableTouchEvent: true,
+                  selectedColor: palette['gray-400'],
+                  selectedTextColor: palette['gray-900'],
+                },
+              }),
+            }}
+            theme={{
+              calendarBackground: palette['gray-200'],
+              backgroundColor: palette['gray-200'],
+              todayTextColor: palette['gray-800'],
+              textDayFontFamily: 'Roboto-Medium',
+              textMonthFontFamily: 'Roboto-Medium',
+            }}
+          />
+        </Box>
+        {selectedDate && (
+          <Stack space={8}>
+            <Text fontWeight="700" fontSize="16" color="gray-900">
+              오전
+            </Text>
+            <Box
+              direction="row"
+              alignX="between"
+              wrap="wrap"
+              style={{ gap: 8 }}>
+              {MORNING_TIMES.map(v => {
+                const isAvailableByRandom = sample([true, false]);
+
+                return (
+                  <TouchableOpacity
+                    key={v}
+                    disabled={!isAvailableByRandom}
+                    style={[
+                      {
+                        borderRadius: 8,
+                        paddingHorizontal: 20,
+                        paddingVertical: 12,
+                        backgroundColor: palette['primary'],
+                      },
+                      !isAvailableByRandom && {
+                        backgroundColor: palette['gray-200'],
+                      },
+                    ]}>
+                    <Text
+                      fontWeight="400"
+                      fontSize="14"
+                      color={isAvailableByRandom ? 'white' : 'gray-400'}>
+                      {v}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </Box>
+          </Stack>
+        )}
+        {selectedDate && (
+          <Stack space={8}>
+            <Text fontWeight="700" fontSize="16" color="gray-900">
+              오후
+            </Text>
+            <Box
+              direction="row"
+              alignX="between"
+              wrap="wrap"
+              style={{ gap: 8 }}>
+              {AFTERNOON_TIMES.map(v => {
+                const isAvailableByRandom = sample([true, false]);
+
+                return (
+                  <TouchableOpacity
+                    key={v}
+                    disabled={!isAvailableByRandom}
+                    style={[
+                      {
+                        borderRadius: 8,
+                        paddingHorizontal: 20,
+                        paddingVertical: 12,
+                        backgroundColor: palette['primary'],
+                      },
+                      !isAvailableByRandom && {
+                        backgroundColor: palette['gray-200'],
+                      },
+                    ]}>
+                    <Text
+                      fontWeight="400"
+                      fontSize="14"
+                      color={isAvailableByRandom ? 'white' : 'gray-400'}>
+                      {v}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              })}
+            </Box>
+          </Stack>
+        )}
+      </Stack>
     );
   });
